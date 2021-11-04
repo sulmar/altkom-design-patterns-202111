e626b9fd29c7d068ec607c02985b862a2f4c9615
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-04 11:31:01 +0100

    Dodanie wzorca Template Method

--- src/DesignPatterns/TemplateMethodPattern/HappyHoursOrderCalculator.cs
+++ src/DesignPatterns/TemplateMethodPattern/HappyHoursOrderCalculator.cs
@@ -1,16 +1,96 @@
-﻿namespace TemplateMethodPattern
+﻿using System;
+
+namespace TemplateMethodPattern
 {
-    // Happy Hours - 10% upustu w godzinach od 9 do 15
-    public class HappyHoursOrderCalculator
+    public interface IOrderCalculator
     {
-        public decimal CalculateDiscount(Order order)
+        decimal CalculateDiscount(Order order);
+    }
+
+
+    // Template Method
+    public abstract class OrderCalculator : IOrderCalculator
+    {
+        public abstract bool CanDiscount(Order order);
+        public abstract decimal GetDiscount(Order order);
+
+        public decimal CalculateDiscount(Order order)   // Template Method
         {
-            if (order.OrderDate.Hour >= 9 && order.OrderDate.Hour <= 15)
+            if (CanDiscount(order))    // Predykat
             {
-                return order.Amount * 0.1m;
+                return GetDiscount(order);     // Upust
             }
             else
-                return 0;
+                return decimal.Zero;
+        }
+    }
+
+    // Happy Hours - 10% upustu w godzinach od 9 do 15
+    public class HappyHoursOrderCalculator : OrderCalculator, IOrderCalculator
+    {
+        private readonly TimeSpan from;
+        private readonly TimeSpan to;
+        private readonly decimal percentage;
+
+        public HappyHoursOrderCalculator(TimeSpan from, TimeSpan to, decimal percentage)
+        {
+            this.from = from;
+            this.to = to;
+            this.percentage = percentage;
+        }
+
+        public override bool CanDiscount(Order order)
+        {
+            return order.OrderDate.TimeOfDay >= from && order.OrderDate.TimeOfDay <= to;
+        }
+
+        public override decimal GetDiscount(Order order)
+        {
+            return order.Amount * percentage;
+        }
+    }
+
+    public class GenderOrderCalculator : OrderCalculator, IOrderCalculator
+    {
+        private readonly Gender gender;
+        private readonly decimal percentage;
+
+        public GenderOrderCalculator(Gender gender, decimal percentage)
+        {
+            this.gender = gender;
+            this.percentage = percentage;
+        }
+
+        public override bool CanDiscount(Order order)
+        {
+            return order.Customer.Gender == gender;
+        }
+
+        public override decimal GetDiscount(Order order)
+        {
+            return order.Amount * percentage;
+        }
+    }
+
+    public class HolidayOrderCalculator : OrderCalculator, IOrderCalculator
+    {
+        private readonly DateTime holiday;
+        private readonly decimal fixedDiscount;
+
+        public HolidayOrderCalculator(DateTime holiday, decimal fixedDiscount)
+        {
+            this.holiday = holiday;
+            this.fixedDiscount = fixedDiscount;
+        }
+
+        public override bool CanDiscount(Order order)
+        {
+            return order.OrderDate == holiday && order.Amount >= fixedDiscount;
+        }
+
+        public override decimal GetDiscount(Order order)
+        {
+            return fixedDiscount;
         }
     }
 
