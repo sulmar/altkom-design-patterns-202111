311de37295ea6c110007f786dae93656cce5917e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-04 13:52:26 +0100

    Dodanie przykładu z Canvas

---
+++ src/DesignPatterns/StatePattern/Canvas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatePattern
+{
+    public class Canvas
+    {
+        public ToolType ToolType { get; set; }
+
+        public void MouseDown()
+        {
+            if (ToolType == ToolType.Selection)
+            {
+                Console.WriteLine("Selection icon");
+            }
+            else if (ToolType == ToolType.Brush)
+            {
+                Console.WriteLine("Brush icon");
+            }
+            else if (ToolType == ToolType.Eraser)
+            {
+                Console.WriteLine("Eraser icon");
+            }
+        }
+
+        public void MouseUp()
+        {
+            if (ToolType == ToolType.Selection)
+            {
+                Console.WriteLine("Draw dashed rectangle");
+            }
+            else if (ToolType == ToolType.Brush)
+            {
+                Console.WriteLine("Draw rectangle");
+            }
+            else if (ToolType == ToolType.Eraser)
+            {
+                Console.WriteLine("Eraser something");
+            }
+        }
+    }
+
+    public enum ToolType
+    {
+        Selection,
+        Brush,
+        Eraser
+    }
+}
