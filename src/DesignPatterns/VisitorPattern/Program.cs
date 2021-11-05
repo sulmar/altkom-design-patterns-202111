4e00b7e89df049d0d07b2fcb19d293f5b72895c5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-11-05 11:35:09 +0100

    Dodanie przykładu Visitator

--- src/DesignPatterns/VisitorPattern/Program.cs
+++ src/DesignPatterns/VisitorPattern/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
 
 namespace VisitorPattern
 {
@@ -12,7 +14,11 @@ namespace VisitorPattern
 
             Form form = Get();
 
-            string html = form.GetHtml();
+            IVisitor visitor = new MarkdownVisitor();
+
+            form.Accept(visitor);
+
+            string html = visitor.Output;
 
             System.IO.File.WriteAllText("index.html", html);
         }
@@ -24,13 +30,13 @@ namespace VisitorPattern
                 Name = "/forms/customers",
                 Title = "Design Patterns",
 
-                Body = new Collection<Control>
+                Body = new Collection<ControlBase>
                 {
 
-                    new Control { Type = ControlType.Label, Caption = "Person", Name = "lblName" },
-                    new Control { Type = ControlType.TextBox, Caption = "FirstName", Name = "txtFirstName", Value = "John"},
-                    new Control { Type = ControlType.Checkbox, Caption = "IsAdult", Name = "chkIsAdult", Value = "true" },
-                    new Control {  Type = ControlType.Button, Caption = "Submit", Name = "btnSubmit", ImageSource = "save.png" },
+                    new LabelControl { Caption = "Person", Name = "lblName" },
+                    new TextBoxControl { Caption = "FirstName", Name = "txtFirstName", Value = "John"},
+                    new CheckBoxControl { Caption = "IsAdult", Name = "chkIsAdult", Value = true },
+                    new ButtonControl {  Caption = "Submit", Name = "btnSubmit", ImageSource = "save.png" },
                 }
 
             };
@@ -41,64 +47,174 @@ namespace VisitorPattern
 
     #region Models
 
-    public class Form
+    public class Form : ControlBase
     {
-        public string Name { get; set; }
         public string Title { get; set; }
-        public ICollection<Control> Body { get; set; }
+        public ICollection<ControlBase> Body { get; set; }
 
-        public string GetHtml()
+        public override void Accept(IVisitor visitor)
         {
-            string html = "<html>";
-
-            html += $"<title>{Title}</title>";
-
-            html += "<body>";
-
-            foreach (var control in Body)
-            {
-                switch (control.Type)
-                {
-                    case ControlType.Label:
-                        html += $"<span>{control.Caption}</span>"; break;
-
-                    case ControlType.TextBox:
-                        html += $"<span>{control.Caption}</span><input type='text' value='{control.Value}'></input>"; break;
-
-                    case ControlType.Checkbox:
-                        html += $"<span>{control.Caption}</span><input type='checkbox' value='{control.Value}'></input>"; break;
-
-                    case ControlType.Button:
-                        html += $"<button><img src='{control.ImageSource}'/>{control.Caption}</button>"; break;
-                }
-
-            }
-
-            html += "</body>";
-            html += "</html>";
-
-            return html;
+            visitor.Visit(this);
         }
     }
 
-    public class Control
-    {
-        public string Name { get; set; }
-        public string Caption { get; set; }
-        public ControlType Type { get; set; }
-        public string Value { get; set; }
-        public string ImageSource { get; set; }
-    }
-
-    public enum ControlType
-    {
-        Label,
-        TextBox,
-        Checkbox,
-        Button
-    }
+  
 
 
     #endregion
+
+    // Abstract Visitor
+    public interface IVisitor
+    {
+        void Visit(Form form);
+        void Visit(LabelControl control);
+        void Visit(TextBoxControl control);
+        void Visit(CheckBoxControl control);
+        void Visit(ButtonControl control);
+
+        string Output { get; }
+    }
+
+    // Concrete Visitor
+
+    public class HtmlVisitor : IVisitor
+    {
+        private readonly StringBuilder builder = new StringBuilder();
+
+        public void Visit(Form form)
+        {
+            string header = "<html>";
+
+            header += $"<title>{form.Title}</title>";
+
+            header += "<body>";
+
+            builder.AppendLine(header);
+
+            foreach (var control in form.Body)
+            {
+                control.Accept(this);                
+            }
+
+            string footer = "</body>";
+            footer += "</html>";
+
+            builder.AppendLine(footer);
+        }
+
+        public void Visit(LabelControl control)
+        {
+            builder.AppendLine($"<span>{control.Caption}</span>");
+        }
+
+        public void Visit(TextBoxControl control)
+        {
+            builder.AppendLine($"<span>{control.Caption}</span><input type='text' value='{control.Value}'></input>");
+        }
+
+        public void Visit(CheckBoxControl control)
+        {
+            builder.AppendLine($"<span>{control.Caption}</span><input type='checkbox' value='{control.Value}'></input>");
+        }
+
+        public void Visit(ButtonControl control)
+        {
+            builder.AppendLine($"<button><img src='{control.ImageSource}'/>{control.Caption}</button>");
+        }
+
+        public string Output => builder.ToString();
+    }
+
+    // Concrete Visitor
+
+    public class MarkdownVisitor : IVisitor
+    {
+        private readonly StringBuilder builder = new StringBuilder();
+
+        public void Visit(Form form)
+        {
+            builder.AppendLine( $"# {form.Title}");
+
+
+            foreach (var control in form.Body)
+            {
+                control.Accept(this);
+            }
+
+        }
+
+        public void Visit(LabelControl control)
+        {
+            builder.AppendLine($"## {control.Caption}");
+        }
+
+        public void Visit(TextBoxControl control)
+        {
+            builder.AppendLine($"**{control.Caption}** _{control.Value}_");
+        }
+
+        public void Visit(CheckBoxControl control)
+        {
+            builder.AppendLine($"**{control.Caption}** _{control.Value}_");
+        }
+
+        public void Visit(ButtonControl control)
+        {
+            builder.AppendLine($"**{control.Caption}** [{control.ImageSource}]");
+        }
+
+        public string Output => builder.ToString();
+
+    }
+
+    // Abstract Element
+    public abstract class ControlBase
+    {
+        public string Name { get; set; }
+        public string Caption { get; set; }
+
+        public abstract void Accept(IVisitor visitor);
+    }
+
+    // Concrete Element
+    public class LabelControl : ControlBase
+    {
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    // Concrete Element
+    public class TextBoxControl : ControlBase
+    {
+        public string Value { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    // Concrete Element
+    public class CheckBoxControl : ControlBase
+    {
+        public bool Value { get; set; }
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    // Concrete Element
+    public class ButtonControl : ControlBase
+    {
+        public string ImageSource { get; set; }
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
 
 }
